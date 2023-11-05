56183bfa55c1299d80122ddfcd55c60df902229d
rajkumar <rajkumaryadav3009@gmail.com>
2023-11-05 12:32:31 +0530

    use ip address instead of localhost for android simulator

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,5 +1,5 @@
 class AppWriteConstants {
   static const String databaseId = '6545f07e6c28d742d0ee';
   static const String projectId = '6541e456c3572343a7a3';
-  static const String endPoint = 'http://localhost:80/v1';
+  static const String endPoint = 'http://192.168.8.104:80/v1';
 }

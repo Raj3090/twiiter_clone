61d9730c3aa59386c2f8a8ce49732c7705a5bd49
rajkumar <rajkumaryadav3009@gmail.com>
2023-11-07 17:12:28 +0530

    create user account provider

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,3 +1,4 @@
+import 'package:appwrite/models.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/apis/auth_api.dart';
@@ -6,11 +7,16 @@ import 'package:twitter_clone/core/utils.dart';
 final authControllerProvider = StateNotifierProvider<AuthController, bool>(
     (ref) => AuthController(authAPI: ref.watch(authApiProvider)));
 
+final userAccountProvider = FutureProvider(
+    (ref) => ref.watch(authControllerProvider.notifier).getCurrentUser());
+
 class AuthController extends StateNotifier<bool> {
   final AuthAPI _authAPI;
   AuthController({required AuthAPI authAPI})
       : _authAPI = authAPI,
         super(false);
+
+  Future<User?> getCurrentUser() => _authAPI.getCurrentUser();
 
   void signUp({
     required String email,
@@ -24,5 +30,19 @@ class AuthController extends StateNotifier<bool> {
     state = false;
     response.fold((error) => showSnackBar(context, error.message),
         (user) => print(user.email));
+  }
+
+  void login({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    state = true;
+
+    var response = await _authAPI.login(email: email, password: password);
+
+    state = false;
+    response.fold((error) => showSnackBar(context, error.message),
+        (user) => print(user.userId));
   }
 }

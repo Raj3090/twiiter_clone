6942a51dddc4df5ed41a6e9327636551af344c5a
rajkumar <rajkumaryadav3009@gmail.com>
2023-11-10 09:08:17 +0530

    move to home screen after login

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -5,6 +5,7 @@ import 'package:twitter_clone/apis/auth_api.dart';
 import 'package:twitter_clone/apis/user_api.dart';
 import 'package:twitter_clone/core/utils.dart';
 import 'package:twitter_clone/features/auth/view/login_view.dart';
+import 'package:twitter_clone/features/home/view/home_view.dart';
 import 'package:twitter_clone/models/user_model.dart';
 
 final authControllerProvider = StateNotifierProvider<AuthController, bool>(
@@ -70,6 +71,6 @@ class AuthController extends StateNotifier<bool> {
 
     state = false;
     response.fold((error) => showSnackBar(context, error.message),
-        (user) => print(user.userId));
+        (user) => Navigator.push(context, HomeView.route()));
   }
 }

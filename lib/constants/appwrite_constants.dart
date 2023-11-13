7ecb72da63ecc2c6ad660e29f82f51842029fe10
rajkumar <rajkumaryadav3009@gmail.com>
2023-11-13 08:58:06 +0530

    update user collection id

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -2,7 +2,5 @@ class AppWriteConstants {
   static const String databaseId = '6545f07e6c28d742d0ee';
   static const String projectId = '6541e456c3572343a7a3';
   static const String endPoint = 'http://192.168.8.104:80/v1';
-  static const String userCollectionId = '654af742902f9fd0cf1e'; 
-
-  
+  static const String userCollectionId = '6552e83b5dc23aa39649';
 }

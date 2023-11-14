13a30d7e40914c5a25a4e6a497e720e16089a8e2
rajkumar <rajkumaryadav3009@gmail.com>
2023-11-14 13:23:54 +0530

    add text input field ,bottom UI

--- lib/features/tweet/views/create_tweet_view.dart
+++ lib/features/tweet/views/create_tweet_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/constants/assets_constants.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/theme/pallete.dart';
 
@@ -16,6 +18,7 @@ class CreateTweetView extends ConsumerStatefulWidget {
 }
 
 class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
+  final tweetTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final currentUser = ref.watch(currentUserDataProvider).value;
@@ -44,14 +47,65 @@ class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
           ? const Loader()
           : SafeArea(
               child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(currentUser.profilePic),
-                  )
-                ],
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(currentUser.profilePic),
+                          radius: 30,
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        Expanded(
+                          child: TextField(
+                            controller: tweetTextController,
+                            style: const TextStyle(
+                              fontSize: 22,
+                            ),
+                            decoration: const InputDecoration(
+                              hintText: 'Whats Happening?',
+                              hintStyle: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              border: InputBorder.none,
+                            ),
+                            maxLines: null,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             )),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(color: Pallete.greyColor, width: 0.3),
+          ),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(left: 16, right: 16),
+              child: SvgPicture.asset(AssetsConstants.galleryIcon),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(left: 16, right: 16),
+              child: SvgPicture.asset(AssetsConstants.gifIcon),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(left: 16, right: 16),
+              child: SvgPicture.asset(AssetsConstants.emojiIcon),
+            )
+          ],
+        ),
+      ),
     );
   }
 }

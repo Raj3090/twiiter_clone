c458795e4998c6bad76ec111ee238b880fe938de
rajkumar <rajkumaryadav3009@gmail.com>
2023-11-19 18:37:38 +0530

    create tweet list widget

---
+++ lib/features/tweet/widgets/tweet_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TweetWidget extends ConsumerWidget {
+  const TweetWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}

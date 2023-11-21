6018541a47d42737bb503afa25aa0b8db19f2045
rajkumar <rajkumaryadav3009@gmail.com>
2023-11-21 19:28:27 +0530

    create tweet list builder

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,7 +1,7 @@
 class AppWriteConstants {
   static const String databaseId = '6545f07e6c28d742d0ee';
   static const String projectId = '6541e456c3572343a7a3';
-  static const String endPoint = 'http://172.20.10.4:80/v1';
+  static const String endPoint = 'http://192.168.8.104:80/v1';
   static const String userCollectionId = '6552e83b5dc23aa39649';
   static const String tweetCollectionId = '6557833323a6199ed74b';
   static const String imagesBucket = '65583068dbcd9ed2ffa8';
--- lib/features/tweet/widgets/tweet_list.dart
+++ lib/features/tweet/widgets/tweet_list.dart
@@ -3,19 +3,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/loading_page.dart';
 import 'package:twitter_clone/features/tweet/controller/tweet_controller.dart';
 
-class TweetWidget extends ConsumerWidget {
-  const TweetWidget({super.key});
+class TweetList extends ConsumerWidget {
+  const TweetList({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ref.watch(tweetListApiProvider).when(
         data: (tweetList) {
-          return ListView.builder(itemBuilder: (context, _) {
-            return Text('some error');
-          });
+          return ListView.builder(
+              itemCount: tweetList.length,
+              itemBuilder: (context, index) {
+                return Text('Tweet UI');
+              });
         },
-        error: (error, StackTrace) {
-          return Text('some error');
+        error: (error, _) {
+          return Text(error.toString());
         },
         loading: () => const Loader());
   }
